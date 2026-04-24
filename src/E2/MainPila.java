01dc20afd4924e9715d1841fb3ce699021384e0d
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-23 21:07:28 -0500

    Agrega la clase MainPila para ejecutar operaciones de pila

---
+++ src/E2/MainPila.java
@@ -0,0 +1,14 @@
+﻿package E2;
+
+public class MainPila {
+    public static void main(String[] args) {
+        PilaTAD pila = new PilaConArreglo(5);
+
+        pila.apilar(10);
+        pila.apilar(20);
+        pila.apilar(30);
+
+        System.out.println("Desapilando: " + pila.desapilar());
+        System.out.println("Desapilando: " + pila.desapilar());
+    }
+}

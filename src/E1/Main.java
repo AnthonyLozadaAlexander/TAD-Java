d1e23c7910d413c18c86074fbfc287f936ed72f4
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-23 21:07:19 -0500

    Agrega la clase Main para ejecutar el contador
    Se implementa la clase Main que crea una instancia de contadorSimple y realiza operaciones de incremento, mostrando el valor del contador en la consola.

---
+++ src/E1/Main.java
@@ -0,0 +1,11 @@
+﻿package E1;
+
+public class Main {
+    public static void main(String[] args) {
+        ContadorTAD contador = new contadorSimple(); // contador = 0;
+        contador.incrementar(); // 0 + 1 = 1
+        contador.incrementar(); // 1 + 1 = 2
+
+        System.out.println("Valor Del Contador: " + contador.obtenerValor());
+    }
+}

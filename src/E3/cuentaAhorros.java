3ed1f733358589ea11768821bbcf7cd371e1ef54
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-23 21:20:34 -0500

    Agrega la clase cuentaAhorros que implementa cuentaBancariaTAD
    Esta clase permite realizar operaciones bancarias como depositar, retirar y consultar el saldo. Se asegura de manejar el saldo de manera adecuada y lanza excepciones en caso de saldo insuficiente.

---
+++ src/E3/cuentaAhorros.java
@@ -0,0 +1,30 @@
+﻿package E3;
+
+public class cuentaAhorros implements cuentaBancariaTAD {
+
+    private double saldo;
+
+    public cuentaAhorros(double saldo){
+        this.saldo = saldo;
+    }
+
+    @Override
+    public double depositar(double monto) {
+        return saldo = saldo + monto;
+    }
+
+    @Override
+    public boolean retirar(double monto) {
+        if(monto > saldo){
+            throw new RuntimeException("Saldo insuficiente");
+        }
+
+        saldo = saldo - monto;
+        return true;
+    }
+
+    @Override
+    public double consultarSaldo(double monto) {
+        return saldo;
+    }
+}

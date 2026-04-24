a17c0449f2123820f4ec8893e912db0ad49d72fc
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-23 21:07:37 -0500

    Agrega la clase PilaConArreglo que implementa PilaTAD
    Esta clase proporciona una implementación de pila utilizando un arreglo,
    incluyendo métodos para apilar, desapilar y verificar si la pila está vacía.

---
+++ src/E2/PilaConArreglo.java
@@ -0,0 +1,39 @@
+﻿package E2;
+
+public class PilaConArreglo implements PilaTAD{
+
+    private int[] datos;
+    private int tope;
+
+    public PilaConArreglo(int capacidad){
+        datos = new int[capacidad];
+        tope = -1;
+    }
+
+
+    @Override
+    public void apilar(int elemento) {
+        if(tope == datos.length - 1){
+            System.out.println("Error: Pila Llena");
+            return;
+        }
+        tope++;
+        datos[tope] = elemento;
+    }
+
+    @Override
+    public int desapilar() {
+        if(estaVacio()){
+            System.out.println("Error: Pila Vacia");
+            return -1;
+        }
+        int elementoArriba = datos[tope];
+        tope--;
+        return elementoArriba;
+    }
+
+    @Override
+    public boolean estaVacio() {
+        return tope == -1;
+    }
+}

3f1861a0d3e60671ee7043cfadb4f4384b7fdfb3
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-23 21:07:02 -0500

    Agrega la clase contadorSimple que implementa ContadorTAD
    Esta clase proporciona métodos para incrementar, decrementar y obtener el valor del contador.

---
+++ src/E1/contadorSimple.java
@@ -0,0 +1,25 @@
+﻿package E1;
+
+public class contadorSimple implements ContadorTAD{
+
+    private int valor;
+
+    public contadorSimple(){
+        this.valor = 0;
+    }
+
+    @Override
+    public void incrementar(){
+        valor++;
+    }
+
+    @Override
+    public void decrementar(){
+        valor--;
+    }
+
+    @Override
+    public int obtenerValor(){
+        return valor;
+    }
+}
